56247f432b8e68c2f876764189649a2901ddfcd3
Jamil. M <jamil.muh777@gmail.com>
2023-03-21 11:40:32 -0500

    Numbers lab Homework

---
+++ src/labs/numberslab.py
@@ -0,0 +1,44 @@
+# Task 1:
+
+zeus = 34_038
+ceasar = 45_838.8734
+alpha = -3_695_365
+beta = 0b_0110_0010_1011
+echo = 0x_174C0
+
+print(zeus)
+print(ceasar)
+print(alpha)
+print(beta)
+print(echo)
+
+
+
+# Task 2:
+
+def numeric_types(value1: int):
+
+    print(bin(value1))
+    print(oct(value1))
+    print(hex(value1))
+    print(value1)
+
+
+# binary
+def basic_binary():
+    print(0b00011101)
+
+# octal
+def basic_octal():
+    print(0o35)
+
+
+# hexadecimal
+def basic_hexadecimal():
+    print(0x1d)
+
+
+numeric_types(29)
+basic_binary()
+basic_octal()
+basic_hexadecimal()
