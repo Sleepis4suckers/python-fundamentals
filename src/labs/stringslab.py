c174aea8bf5ce9c6ea32b4d3b012748ee6052826
Jamil. M <jamil.muh777@gmail.com>
2023-03-22 17:43:38 -0500

    Strings Lab Homework

---
+++ src/labs/stringslab.py
@@ -0,0 +1,48 @@
+# Task 1:
+
+nibiru: str = 'This statement has empty spaces '
+
+def get_pos_value():
+    print(nibiru[6:17])
+
+def get_neg_value():
+    print(nibiru[-17:-6])
+
+get_pos_value()
+get_neg_value()
+
+
+
+# Task 2:
+homework_task: str = ' This statement has empty spaces '
+
+# strip()
+print(homework_task.strip())
+
+# upper()
+print(homework_task.upper())
+
+# find()
+print(homework_task.find('eme'))
+
+# count()
+print(homework_task.count('s'))
+
+
+
+
+# Task 3:
+""" Jack and Jill poem"""
+def poem_escape_seq():
+    value_n: str = ' Jack and Jill went up the hill, \n To fetch a pail of water; \n Jack fell down, and broke his crown.\
+     \n And Jill came tumbling after.'
+    print(value_n)
+
+
+poem_escape_seq()
+
+
+
+
+
+
