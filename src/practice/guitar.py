727071df3ce9c08e4ac002be6e0b405ec03faf6b
Jamil. M <jamil.muh777@gmail.com>
2023-04-06 12:15:26 -0500

    Guitar Practice

---
+++ src/practice/guitar.py
@@ -0,0 +1,26 @@
+""" Practice with classes and inheritance """
+
+class Guitar:
+    def __init__(self, strings_num, body_style):
+        self._strings_num = strings_num
+        self._body_style = body_style
+
+    def play(self):
+        print(f'{self._strings_num} string {self._body_style } guitar is fun to play.')
+
+
+class Mandolin(Guitar):
+    def __init__(self):
+        super().__init__(8, 'bowl shaped')
+
+
+def main():
+    my_guitar = Guitar(6, 'Round back')
+    my_guitar.play()
+
+    my_mando = Mandolin()
+    my_mando.play()
+
+
+if __name__ == '__main__':
+    main()
